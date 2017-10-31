fbd14e9eab8ecaf1204afb0410631911c85597c9
Tri Le <lehuutri@gmail.com>
2017-10-31 14:42:57 +0700

    Initial Commit

---
+++ MonthCalendar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MonthCalendar
+//
+//  Created by Tri Le on 10/31/17.
+//  Copyright © 2017 Tri Le. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
